b04bccd8ae979ce02015527353d64b45be899aa3
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-09-11 20:06:27 +0530

    gzosp

--- gzosp.mk
+++ gzosp.mk
@@ -4,15 +4,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/kinzie/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_kinzie
-PRODUCT_BRAND := Android
+PRODUCT_NAME := gzosp_kinzie
+PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := kinzie
-PRODUCT_MODEL := XT1581
+PRODUCT_MODEL := XT1580
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
