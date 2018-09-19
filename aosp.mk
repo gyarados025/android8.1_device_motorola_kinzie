fd534a55f5e98ea731023503ba78e430900bbe90
Kshitij Kumar <40200770+gyarados025@users.noreply.github.com>
2018-09-19 19:27:56 +0530

    aosp

--- aosp.mk
+++ aosp.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/kinzie/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := gzosp_kinzie
+PRODUCT_NAME := aosp_kinzie
 PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := kinzie
 PRODUCT_MODEL := XT1580
