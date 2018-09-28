270e9405b1886c4b40fe822cfb2e0c2883b6311e
Kshitij Kumar <kkghoshit@gmail.com>
2018-09-28 16:20:34 +0530

    CMActions -> LineageActions

--- device.mk
+++ device.mk
@@ -73,10 +73,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/com.motorola.cameraone.xml:system/etc/permissions/com.motorola.cameraone.xml
 
-# CMActions
+# LineageActions
 PRODUCT_PACKAGES += \
-    CMActions \
-    libjni_CMActions
+	LineageActions
 
 # Display
 PRODUCT_AAPT_CONFIG := normal
